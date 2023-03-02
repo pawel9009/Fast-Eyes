acef0094bcdc75845c2c0a50d0867573fdda9fb8
pawel9009 <pawel.b512222@gmail.com>
2023-03-02 21:06:24 +0100

    add new model

--- project/app/models.py
+++ project/app/models.py
@@ -1,6 +1,7 @@
 import uuid
-
+import datetime
 from django.db import models
+from accounts.models import User
 
 from mysite import settings
 
@@ -13,3 +14,18 @@ class Image(models.Model):
     def __str__(self) -> str:
 
         return f'{self.name} {self.uuid}'
+    
+
+class Experiment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    date = models.DateField(default=datetime.datetime.now)
+    correct = models.IntegerField()
+    incorrect = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.user_id} {self.date}'
+
+
+
+
+
