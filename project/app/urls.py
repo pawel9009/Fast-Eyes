3dec38932bd438faf7e2f0d3bc6ff2ead4582116
pawel9009 <pawel.b512222@gmail.com>
2023-03-10 17:52:11 +0100

    add alt image when sample is hidden

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import ExperimentView, upload_images, ExperimentListView
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('data/', upload_images, name='data'),
     path('experiment/', ExperimentView.as_view(), name='experiment'),
     path('list/', ExperimentListView.as_view(), name='exp_list'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
