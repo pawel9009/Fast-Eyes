65a35dc61e61571e278113e7825c5be18bdbfb04
pawel9009 <pawel.b512222@gmail.com>
2023-03-17 15:25:45 +0100

    add login_required

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.conf.urls.static import static
 
 from .views import ExperimentView, upload_images, ExperimentListView, ChallengeView
@@ -7,8 +8,8 @@ from .views import ExperimentView, upload_images, ExperimentListView, ChallengeV
 app_name = 'app'
 
 urlpatterns = [
-    path('data/', upload_images, name='data'),
-    path('experiment/', ExperimentView.as_view(), name='experiment'),
-    path('challenge/', ChallengeView.as_view(), name='challenge'),
-    path('list/', ExperimentListView.as_view(), name='exp_list'),
+    path('data/', login_required(upload_images), name='data'),
+    path('experiment/', login_required(ExperimentView.as_view()), name='experiment'),
+    path('challenge/', login_required(ChallengeView.as_view()), name='challenge'),
+    path('list/', login_required(ExperimentListView.as_view()), name='exp_list'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
