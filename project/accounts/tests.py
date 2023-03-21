16f537f1857145623f9c25e030bb513798d2b327
pawel9009 <pawel.b512222@gmail.com>
2023-03-21 20:10:45 +0100

    add first tests

---
+++ project/accounts/tests.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import User
+from django.test import Client
+from django.contrib.auth import get_user_model
+
+
+
+class UserRegistrationTestCase(TestCase):
+    def setUp(self) -> None:
+        self.cl = Client()
+        self.url = reverse('accounts:signup')
+        self.username=  'user999',
+        self.password1= 'zaq1@WSX',
+        self.password2=  'zaq1@WSX',
+        self.birth_day= 1934,
+        self.sex= 'Male',
+        self.ailments='sdadasd',
+        return super().setUp()
+        
+
+    def test_registration(self):
+        response = self.client.post(self.url, {
+            'username': self.username,
+            'password1': self.password1,
+            'password2':  self.password2,
+            'birth_day':  self.birth_day,
+            'sex': self.sex,
+            'ailments':  self.ailments,
+        }, format='text/html')
+
+        # self.assertEqual(response.status_code, 200)
+        User = get_user_model()
+        user = User.objects.get(username=self.username)
+        self.assertIsNotNone(user)
+
+      
