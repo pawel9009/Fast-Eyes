389ebc812b1172a5ca67cda1a87ff1fdd6553dcc
pawel9009 <pawel.b512222@gmail.com>
2023-03-28 16:11:59 +0200

    add working barplot for challenge results

--- project/statistic/utils.py
+++ project/statistic/utils.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 from io import BytesIO
+import numpy as np
 import base64
 
-def plot():
-    x = [1, 2, 3, 4, 5]
-    y = [10, 8, 6, 4, 2]
+def plot(data):
+    x = [float(q) for q in range(5,101,5)]
+    pr_dict= {key:0 for key in x}
+
+    for item in data:
+        pr_dict[item.pass_rate]+=1
+    
+
+    y = [value for _,value in pr_dict.items()]
+
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x, y=y, mode='lines'))
+    fig.add_trace(go.Bar(x=x, y=y))
+    fig.update_xaxes(type='category')
+    fig.update_layout(
+        
+        title="Challenge Barplot",
+        xaxis_title="Pass rate (%)",
+        yaxis_title="Counts",)
 
     context = {'plot': fig.to_html(full_html=False)}
 
--- project/statistic/views.py
+++ project/statistic/views.py
@@ -2,23 +2,16 @@ from django.shortcuts import render
 from django.views import generic
 # Create your views here.
 from .utils import plot
-
-import matplotlib.pyplot as plt
-import pandas as pd
-from django.http import HttpResponse
-import seaborn as sns
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from django.template import loader
-
-import base64
+from app.models import Experiment
 
 
 class HomeView(generic.TemplateView):
     template_name = 'statistic/home.html'
 
     def get(self, request, *args, **kwargs):
+        data = Experiment.objects.all().filter(challenge=True)
         
-        context = plot()
+        context = plot(data)
         
         return render(request, self.template_name, context)
       
