ecdfbf8e947f89e8c70658c9f848f629e6c829e0
pawel9009 <pawel.b512222@gmail.com>
2023-04-04 20:49:10 +0200

    add timer in challenge

--- project/mysite/settings.py
+++ project/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*)euwgc^(8*p5b&&+p5b&c85trxgsdd%8#xm6407d^i3zs&qss'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
