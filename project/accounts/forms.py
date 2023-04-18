091548761b1ff1ce325968657b6d0031fc126087
pawel9009 <pawel.b512222@gmail.com>
2023-04-19 01:42:28 +0200

    add new css

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -15,9 +15,12 @@ class UserCreateForm(UserCreationForm):
         self.fields['ailments'].label = 'Diseases'
         self.fields['username'].help_text = None
         self.fields['password1'].help_text = None
+
+        
         self.fields['password2'].help_text = None
         self.fields['ailments'].widget = forms.Textarea(attrs={'rows': 2, 'cols': 50})
         self.fields['username'].widget = forms.TextInput(attrs={'placeholder': ''})
-        self.fields['password1'].widget = forms.TextInput(attrs={'placeholder': ''})
-        self.fields['password2'].widget = forms.TextInput(attrs={'placeholder': ''})
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'placeholder': ''})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'placeholder': ''})
         self.fields['ailments'].widget = forms.TextInput(attrs={'placeholder': ''})
+
